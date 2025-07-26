2197f88a528db4683d9146c49e7117fd237ad405
hekawena <tongw577@gmail.com>
2025-07-26 15:32:41 +0800

    初版

---
+++ My_NewChassis/Application/Tasks/Inc/Chassis_Task.h
@@ -0,0 +1,142 @@
+// /**
+//   ****************************(C) COPYRIGHT 2016 DJI****************************
+//   * @file       chassis_behaviour.c/h
+//   * @brief      完成底盘行为任务。
+//   * @note
+//   * @history
+//   *  Version    Date            Author          Modification
+//   *  V1.0.0     Dec-26-2018     RM              1. 完成
+//   *  v2.0.0     Nov-05-2023     pxx             刚起步
+//   *
+//   @verbatim
+//   ==============================================================================
+
+//   ==============================================================================
+//   @endverbatim
+//   ****************************(C) COPYRIGHT 2016 DJI****************************
+//   */
+#ifndef CHASSIS_TASK_H
+#define CHASSIS_TASK_H
+
+#include "chassis_behaviour.h"
+#include "main.h"
+#include "remote_control.h"
+#include "old_pid.h"
+
+#include "user_lib.h"
+//#include "observe_task.h"
+// #include "pid.h"
+
+// /*底盘CAN_ID
+//2         1
+//3         4
+
+// 任务开始空闲一段时间
+#define CHASSIS_TASK_INIT_TIME 357
+
+//遥控器前进摇杆（max 660）转化成车体前进速度（m/s）的比例
+#define CHASSIS_VX_RC_SEN 0.005f
+//遥控器左右摇杆（max 660）转化成车体左右速度（m/s）的比例
+#define CHASSIS_VY_RC_SEN 0.005f
+//遥控器左右摇杆（max 660）转化成车体左右速度（m/s）的比例
+#define CHASSIS_WZ_RC_SEN 0.004f
+//底盘前后左右控制按键
+#define CHASSIS_FRONT_KEY KEY_PRESSED_OFFSET_W
+#define CHASSIS_BACK_KEY KEY_PRESSED_OFFSET_S
+#define CHASSIS_LEFT_KEY KEY_PRESSED_OFFSET_A
+#define CHASSIS_RIGHT_KEY KEY_PRESSED_OFFSET_D
+
+#define CHASSIS_ROS_TO_WZ 0.0021f
+
+//底盘设置旋转速度，设置前后左右轮不同设定速度的比例分权 0为在几何中心，不需要补偿
+#define CHASSIS_WZ_SET_SCALE 0.0f
+//底盘电机最大速度
+#define MAX_WHEEL_SPEED 3.0f
+
+// #define CHASSIS_ACCEL_X_NUM 0.1666666667f
+// #define CHASSIS_SPEED_NUM 0.05f
+// // #define CHASSIS_ACCEL_Y_NUM 0.3333333333f
+
+// 遥控遥感死区限制
+#define CHASSIS_RC_DEADLINE 10
+
+// 底盘任务控制间隔 2ms
+#define CHASSIS_CONTROL_TIME_MS 2
+// 底盘任务控制间隔 0.002s
+#define CHASSIS_CONTROL_TIME 0.002f
+// 底盘任务控制频率，尚未使用这个宏
+#define CHASSIS_CONTROL_FREQUENCE 500.0f
+
+// 底盘电机最大速度
+// #define MAX_WHEEL_SPEED 1.0f
+//底盘运动过程最大前进速度
+#define NORMAL_MAX_CHASSIS_SPEED_X 2.0f
+//底盘运动过程最大平移速度
+#define NORMAL_MAX_CHASSIS_SPEED_Y 2.0f
+
+#define CHASSIS_MOTOR_W_TO_VECTOR_SEN WHEEL_R
+#define WHEELR 0.075f  //7.5cm
+
+#define MOTOR_SPEED_TO_CHASSIS_SPEED_VX 0.25f
+#define MOTOR_SPEED_TO_CHASSIS_SPEED_VY 0.25f
+#define MOTOR_SPEED_TO_CHASSIS_SPEED_WZ 0.25f
+
+#define MOTOR_DISTANCE_TO_CENTER 0.2f
+
+#define CHASSIS_ACCEL_X_NUM 0.1666666667f
+#define CHASSIS_ACCEL_Y_NUM 0.3333333333f
+
+//小陀螺旋转速度
+#define ROTATION_SPEED_MAX 6.0f
+#define ROTATION_SPEED_ADD_VALUE ROTATION_SPEED_MAX/3
+
+//底盘3508最大can发送电流值
+#define MAX_MOTOR_CAN_CURRENT 16000.0f
+
+//底盘电机速度环PID
+#define M3505_MOTOR_SPEED_PID_KP 15000.0f
+#define M3505_MOTOR_SPEED_PID_KI 10.0f
+#define M3505_MOTOR_SPEED_PID_KD 0.0f
+#define M3505_MOTOR_SPEED_PID_MAX_OUT MAX_MOTOR_CAN_CURRENT
+#define M3505_MOTOR_SPEED_PID_MAX_IOUT 2000.0f
+
+typedef struct 
+{
+	float vx;
+	float vy;
+	float wz;
+}chassis_vel_t;
+
+typedef struct
+{
+	const Remote_Info_Typedef *chassis_RC;	  // 底盘使用的遥控器指针
+	const control_mode_t* rc_control_mode;
+
+	//
+	dji_motor_measure_t* chassis_motor[4];
+	PidTypeDef motor_speed_pid[4];             //底盘电机速度pid
+	chassis_vel_t vel_ref;
+	chassis_vel_t vel_set;
+	//
+
+	float chassis_yaw;
+
+	first_order_filter_type_t chassis_cmd_slow_set_vx;
+  	first_order_filter_type_t chassis_cmd_slow_set_vy;
+
+	ramp_function_source_t rotation_ramp_wz;   //小陀螺斜坡函数缓启动 停止
+ 	bool_t rotation_diraction;                 //小陀螺旋转的方向
+
+  	fp32 vx_max_speed;  //前进方向最大速度 单位m/s
+  	fp32 vx_min_speed;  //前进方向最小速度 单位m/s
+  	fp32 vy_max_speed;  //左右方向最大速度 单位m/s
+  	fp32 vy_min_speed;  //左右方向最小速度 单位m/s
+	
+	fp32 tmp;
+} chassis_move_t;
+
+extern void Chassis_Task(void const *pvParameters);
+
+extern chassis_move_t chassis_move;
+chassis_move_t* get_chassis_point();
+#endif
---
+++ My_NewChassis/Application/Tasks/Src/Can_Task.c
@@ -0,0 +1,141 @@
+#include "main.h"
+#include "can_task.h"
+#include "Chassis_Task.h"
+#include "old_pid.h"
+
+//#define DEBUG
+static void Damiao_Motor_Enable();
+static void Damiao_Motor_CAN_Send(float Postion, float Velocity, float KP, float KD, float Torque);
+static void Dji_Motor_Shoot_Can_Send(int16_t current_Shoot_Left,int16_t current_Shoot_Right,int16_t current_Shoot_Pull);
+static void Dji_Motor_Chassis_Can_Send(int16_t current_id_1,int16_t current_id_2,int16_t current_id_3,int16_t current_id_4);
+static void Dji_Motor_Pitch_Can_Send(int16_t current_pitch);
+
+extern CAN_TxFrameTypeDef ALLShootTxFrame;
+extern CAN_TxFrameTypeDef ALLChassisTxFrame;
+extern CAN_TxFrameTypeDef YAW_MOTOR_FRAME;
+extern CAN_TxFrameTypeDef PITCH_MOTOR_FRAME;
+
+void Can_Task(void const * argument)
+{
+  /* USER CODE BEGIN Can_Task */
+  /* Infinite loop */
+  // extern gimbal_t gimbal;
+  // chassis_move_t* can_chassis_move = get_chassis_point();
+  osDelay(800);
+  // Damiao_Motor_Enable();
+  osDelay(10);
+  // Damiao_Motor_Enable();
+  osDelay(10);
+
+  for(;;)
+  {
+  	// 仅作LED显示
+  	 // HAL_GPIO_TogglePin(LED_R_GPIO_Port, LED_R_Pin);
+  	 // if (remote_ctrl.rc.s[0]== 0)
+  	 // {
+  	 // 	HAL_GPIO_WritePin(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_SET);
+  	 // 	HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, GPIO_PIN_RESET);
+  	 // }
+  	 // else
+  	 // {
+  	 // 	HAL_GPIO_WritePin(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_RESET);
+  	 // 	HAL_GPIO_WritePin(LED_B_GPIO_Port, LED_B_Pin, GPIO_PIN_SET);
+  	 // }
+     // //摩擦轮加拨蛋
+     // Dji_Motor_Shoot_Can_Send(gimbal.gimbal_shoot.shoot_motor_left->target_current,\
+     //                           gimbal.gimbal_shoot.shoot_motor_right->target_current,\
+     //                           gimbal.gimbal_shoot.pull_motor->target_current);
+    // 摩擦轮加拨蛋加pitch
+     // Dji_Motor_Shoot_Can_Send(0,0,0,0);
+    // 四个轮子
+     Dji_Motor_Chassis_Can_Send(chassis_move.chassis_motor[0]->target_current,\
+                                 chassis_move.chassis_motor[1]->target_current,\
+                                 chassis_move.chassis_motor[2]->target_current,\
+                                 chassis_move.chassis_motor[3]->target_current);
+    // Dji_Motor_Pitch_Can_Send(gimbal.gimbal_pos.pitch_motor->target_current);
+    // Dji_Motor_Pitch_Can_Send(0);//
+    //// 单独的达妙YAW
+    // Damiao_Motor_Enable();
+    // Dji_Motor_Chassis_Can_Send(0,0,0,0);
+    // Damiao_Motor_Enable();
+    // Damiao_Motor_CAN_Send(0,0,0,0,0);
+    // Dji_Motor_Shoot_Can_Send(500,500,500);
+
+    osDelay(2);
+  }
+  /* USER CODE END Can_Task */
+}
+
+static int float_to_uint(float x, float x_min, float x_max, int bits){
+    float span = x_max-x_min;
+    float offset =x_min;
+    return (int) ((x-offset)*((float)((1<<bits)-1))/span);
+}
+
+static void Damiao_Motor_CAN_Send(float Postion, float Velocity, float KP, float KD, float Torque){
+  static uint16_t Postion_Tmp,Velocity_Tmp,Torque_Tmp,KP_Tmp,KD_Tmp;
+
+  Postion_Tmp  =  float_to_uint(Postion,-12.5f,12.5,16) ;
+  Velocity_Tmp =  float_to_uint(Velocity,-30,30,12);
+  KP_Tmp = float_to_uint(KP,0,500,12);
+  KD_Tmp = float_to_uint(KD,0,5,12);
+  Torque_Tmp = float_to_uint(Torque,-10,10,12);
+	YAW_MOTOR_FRAME.Data[0] = (uint8_t)(Postion_Tmp>>8);
+	YAW_MOTOR_FRAME.Data[1] = (uint8_t)(Postion_Tmp);
+	YAW_MOTOR_FRAME.Data[2] = (uint8_t)(Velocity_Tmp>>4);
+	YAW_MOTOR_FRAME.Data[3] = (uint8_t)((Velocity_Tmp&0x0F)<<4) | (uint8_t)(KP_Tmp>>8);
+	YAW_MOTOR_FRAME.Data[4] = (uint8_t)(KP_Tmp);
+	YAW_MOTOR_FRAME.Data[5] = (uint8_t)(KD_Tmp>>4);
+	YAW_MOTOR_FRAME.Data[6] = (uint8_t)((KD_Tmp&0x0F)<<4) | (uint8_t)(Torque_Tmp>>8);
+	YAW_MOTOR_FRAME.Data[7] = (uint8_t)(Torque_Tmp);
+  USER_CAN_TxMessage(&YAW_MOTOR_FRAME);
+}
+static void Damiao_Motor_Enable(){
+
+	YAW_MOTOR_FRAME.Data[0] = 0xFF;
+	YAW_MOTOR_FRAME.Data[1] = 0xFF;
+	YAW_MOTOR_FRAME.Data[2] = 0xFF;
+	YAW_MOTOR_FRAME.Data[3] = 0xFF;
+	YAW_MOTOR_FRAME.Data[4] = 0xFF;
+	YAW_MOTOR_FRAME.Data[5] = 0xFF;
+	YAW_MOTOR_FRAME.Data[6] = 0xFF;
+	YAW_MOTOR_FRAME.Data[7] = 0xFC;
+
+  USER_CAN_TxMessage(&YAW_MOTOR_FRAME);
+}
+
+static void Dji_Motor_Pitch_Can_Send(int16_t current_pitch){
+  PITCH_MOTOR_FRAME.Data[0] = 0;
+  PITCH_MOTOR_FRAME.Data[1] = 0;
+  PITCH_MOTOR_FRAME.Data[2] = 0;
+  PITCH_MOTOR_FRAME.Data[3] = 0;
+  PITCH_MOTOR_FRAME.Data[4] = 0;
+  PITCH_MOTOR_FRAME.Data[5] = 0;
+  PITCH_MOTOR_FRAME.Data[6] = current_pitch >> 8;
+  PITCH_MOTOR_FRAME.Data[7] = current_pitch;
+  USER_CAN_TxMessage(&PITCH_MOTOR_FRAME);
+}
+
+static void Dji_Motor_Shoot_Can_Send(int16_t current_Shoot_Left,int16_t current_Shoot_Right,int16_t current_Shoot_Pull){
+    ALLShootTxFrame.Data[0] = current_Shoot_Left>>8;
+    ALLShootTxFrame.Data[1] = current_Shoot_Left;
+    ALLShootTxFrame.Data[2] = current_Shoot_Right>>8;
+    ALLShootTxFrame.Data[3] = current_Shoot_Right;
+    ALLShootTxFrame.Data[4] = current_Shoot_Pull>>8;
+    ALLShootTxFrame.Data[5] = current_Shoot_Pull;
+    ALLShootTxFrame.Data[6] = 0;
+    ALLShootTxFrame.Data[7] = 0;
+    USER_CAN_TxMessage(&ALLShootTxFrame);
+}
+static void Dji_Motor_Chassis_Can_Send(int16_t current_id_1,int16_t current_id_2,int16_t current_id_3,int16_t current_id_4){
+    ALLChassisTxFrame.Data[0] = current_id_1  >>8;
+    ALLChassisTxFrame.Data[1] = current_id_1;
+    ALLChassisTxFrame.Data[2] = current_id_2  >>8;
+    ALLChassisTxFrame.Data[3] = current_id_2;
+    ALLChassisTxFrame.Data[4] = current_id_3  >>8;
+    ALLChassisTxFrame.Data[5] = current_id_3;
+    ALLChassisTxFrame.Data[6] = current_id_4  >>8;
+    ALLChassisTxFrame.Data[7] = current_id_4;
+
+    USER_CAN_TxMessage(&ALLChassisTxFrame);
+}
